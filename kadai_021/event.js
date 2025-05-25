e495660fd7d29572f86e9bca29a0e6da9884cc17
kiiii1279 <yuki105202305@gmail.com>
2025-05-25 12:27:21 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn')
+const text =document.getElementById('text')
+
+btn.addEventListener('ボタンをクリックした', () => {
+
+})
+
+setTimeout(() => {
+    console.log('ボタンをクリックした')
+})
